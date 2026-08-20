70f088f5c3eb04bc4fbc8cebb3ef707f67e40c2d
eljandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-08-20 17:48:23 +0200

    Implement Trainer class initialization for distributed training setup

--- src/train.py
+++ src/train.py
@@ -1,8 +1,11 @@
 import contextlib
+import os
 from collections.abc import Callable
+from datetime import timedelta
 
 import torch
 from torch.distributed.checkpoint.stateful import Stateful
+from torch.distributed.elastic.multiprocessing.errors import record
 
 from src.components.checkpoint import CheckpointManager  # type: ignore
 from src.components.dataloader import BaseDataLoader  # type: ignore
@@ -12,8 +15,9 @@ from src.components.metrics import MetricsProcessor  # type: ignore
 from src.components.optimizer import OptimizersContainer  # type: ignore
 from src.components.tokenizer import DeepSeekV3Tokenizer  # type: ignore
 from src.config import JobConfig
+from src.config.job_config import Parallelism
 from src.distributed import ParallelDims  # type: ignore
-from src.tools import utils  # type: ignore
+from src.tools import device_utils, utils  # type: ignore
 
 
 class Trainer(Stateful):
@@ -39,3 +43,33 @@ class Trainer(Stateful):
 
     step: int
     ntokens_seen: int
+
+
+    @record
+    def __init__(self, job_config: JobConfig):
+        self.job_config = job_config
+
+        device_module, device_type = (
+            device_utils.device_module,
+            device_utils.device_type,
+        )
+        self.device = torch.device(f"{device_type}:{int(os.environ['LOCAL_RANK'])}")
+        device_module.set_device(self.device)
+
+        # init distributed and build meshes
+        torch.distributed.init_process_group(
+            backend="nccl",
+            timeout=timedelta(seconds=job_config.comm.init_timeout_seconds),
+        )
+        world_size = int(os.environ["WORLD_SIZE"])
+        parallelism_config = job_config.parallelism
+        self.parallel_dims = parallel_dims = self._create_parallel_dims(
+            parallelism_config, world_size
+        )
+
+
+
+    def _create_parallel_dims(
+        self, parallelism_config: Parallelism, world_size: int
+    ) -> ParallelDims:
+        pass
