6b8942a7ccae26f0165415de22fbf3399acd0b2e
eljandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-08-28 11:17:38 +0200

    feat(parallelize): add parallelization functions for DeepSeekV3 model

--- src/distributed/model_parallel.py
+++ src/distributed/model_parallel.py
@@ -130,7 +130,7 @@ def apply_fsdp(
     next_transformer_blocks = transformer_blocks[1:] + [None]
 
     if tok_embeddings is not None and len(layers) > 0:
-        tok_embeddings.set_modules_to_forward_prefetch([transformer_blocks[0]])  # ty:ignore[call-non-callable, unresolved-attribute]
+        tok_embeddings.set_modules_to_forward_prefetch([transformer_blocks[0]])  
 
     for transformer_block, next_transformer_block in zip(
         transformer_blocks, next_transformer_blocks
@@ -152,7 +152,7 @@ def apply_fsdp(
     prev_transformer_blocks = reversed_transformer_blocks[1:] + [None]
 
     if norm is not None and output is not None and len(layers) > 0:
-        output.set_modules_to_backward_prefetch([reversed_transformer_blocks[0]])  # ty:ignore[call-non-callable, unresolved-attribute]
+        output.set_modules_to_backward_prefetch([reversed_transformer_blocks[0]])  
 
     for transformer_block, prev_transformer_block in zip(
         reversed_transformer_blocks, prev_transformer_blocks
